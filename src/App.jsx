1d9714e0a2721295533b1c488884ddece70e25ac
SimranGrak <simrangrak1761@gmail.com>
2024-05-17 02:05:48 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Nav from "./Components/Nav";
+import Hero from "./Components/Hero";
+import "./App.css";
+
+const App=()=>{
+  return (
+   <div>
+    <Nav/>
+    <Hero/>
+   </div>
+  );
+};
+
+export default App
